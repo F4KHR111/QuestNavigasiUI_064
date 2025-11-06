4c10eb17eda42c1697641119671832c1a4d73d0e
F4KHR111 <fakhriwirdiyan05@gmail.com>
2025-11-06 12:07:30 +0700

    Membuat Composable function `DataApp`

--- app/src/main/java/com/example/navigasi/Navigasi.kt
+++ app/src/main/java/com/example/navigasi/Navigasi.kt
@@ -1,8 +1,22 @@
 package com.example.navigasi
 
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+
 
 enum class Navigasi {
     Formulirku,
     Data
 }
 
+@Composable
+fun DataApp(
+    navController: NavHostController = rememberNavController(),
+    modifier: Modifier
+) {
+    Scaffold{isiRuang ->
+    }
+}
