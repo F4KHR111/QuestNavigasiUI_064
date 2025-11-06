3bd48d816c999ab334ec2d2fdfe7580c8e6ce7d0
F4KHR111 <fakhriwirdiyan05@gmail.com>
2025-11-06 13:12:10 +0700

    Menambahkan parameter pada composable `FormIsian`

--- app/src/main/java/com/example/navigasi/view/Formulir.kt
+++ app/src/main/java/com/example/navigasi/view/Formulir.kt
@@ -2,9 +2,13 @@
 
 package com.example.navigasi.view
 
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 
 @Composable
 fun FormIsian(
-
-)
+    jenisK:List<String> = listOf("Laki-laki","Perempuan"),
+    OnSubmitBtnClick : () -> Unit
+){
+    
+}
