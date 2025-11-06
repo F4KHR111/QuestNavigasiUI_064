41c38e889615189f6e6164e1142157e3d912aea6
F4KHR111 <fakhriwirdiyan05@gmail.com>
2025-11-06 18:01:31 +0700

    Membuat halaman TampilData

--- app/src/main/java/com/example/navigasi/view/TampilanData.kt
+++ app/src/main/java/com/example/navigasi/view/TampilanData.kt
@@ -1,7 +1,16 @@
 package com.example.navigasi.view
 
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import com.example.navigasi.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 
@@ -9,5 +18,23 @@ import androidx.compose.runtime.Composable
 fun TampilData(
     onBackClick:()->Unit
 ){
-
+    val items = listOf(
+        Pair(stringResource(id = R.string.nama_lengkap), "Contoh Nama"),
+        Pair(stringResource(id = R.string.jenis_kelamin), "Lainnya"),
+        Pair(stringResource(id = R.string.alamat), "Yogyakarta"),
+    )
+    Scaffold (modifier = Modifier,
+        {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = stringResource(id= R.string.tampil),
+                        color = Color.White
+                    )},
+                colors = TopAppBarDefaults
+                    .mediumTopAppBarColors(colorResource(id=R.color.teal_700))
+            )
+        }){
+        
+    }
 }
