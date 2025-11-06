b0fe85de0d698d9045b117589fb89702cb8eec62
F4KHR111 <fakhriwirdiyan05@gmail.com>
2025-11-06 17:26:48 +0700

    Menambahkan `OutlinedTextField` untuk `Nama Lengkap`

--- app/src/main/java/com/example/navigasi/view/Formulir.kt
+++ app/src/main/java/com/example/navigasi/view/Formulir.kt
@@ -5,7 +5,9 @@ package com.example.navigasi.view
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -16,6 +18,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.example.navigasi.R
 
 @Composable
@@ -43,7 +46,15 @@ fun FormIsian(
             horizontalAlignment = Alignment.CenterHorizontally
         )
         {
-
+            OutlinedTextField(
+                value = "",
+                singleLine = true,
+                modifier = Modifier
+                    .padding(top = 20.dp)
+                    .width(250.dp),
+                label = {Text(text = "Nama Lengkap")},
+                onValueChange = {},
+            )
         }
     }
 }
